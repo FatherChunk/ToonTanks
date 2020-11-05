5a7a1a0dfeaf37edcaa9b17233dad3073d0e122d
fatherchunk99 <j_grainger@hotmail.co.uk>
2020-11-05 09:37:10 +0000

    Health component created, gamemode expanded

--- Source/ToonTanks/Pawns/PawnBase.h
+++ Source/ToonTanks/Pawns/PawnBase.h
@@ -31,16 +31,14 @@ private:
 public:
 	// Sets default values for this pawn's properties
 	APawnBase();
+	void PawnDestroyed();
+
+	virtual void HandleDestruction();
 
 protected:
 
 	void RotateTurret(FVector LookAtTarget);
 
 	void Fire();
-
-	virtual void HandleDestruction(); 
-
-
-
 
 };
